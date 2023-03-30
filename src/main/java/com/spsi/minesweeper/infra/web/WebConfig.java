2b3a5f7bb10dba6058ad1da81d6822ec108ff35c
SPSilicon <dlsrb915@gmail.com>
2023-03-30 19:19:39 +0900

    add cors setting

--- src/main/java/com/spsi/minesweeper/infra/web/WebConfig.java
+++ src/main/java/com/spsi/minesweeper/infra/web/WebConfig.java
@@ -5,18 +5,22 @@ import com.spsi.minesweeper.controller.GameWebSocketHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.rsocket.annotation.support.RSocketMessageHandler;
 import org.springframework.web.reactive.HandlerMapping;
+import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
 import org.springframework.web.reactive.socket.WebSocketHandler;
 import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
+import org.springframework.web.util.pattern.PathPatternRouteMatcher;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 @EnableWebFlux
-public class WebConfig {
+public class WebConfig implements WebFluxConfigurer{
     @Autowired
     private GameWebSocketHandler gameWebSocketHandler;
 
@@ -26,6 +30,14 @@ public class WebConfig {
         return new WebSocketHandlerAdapter();
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+          .allowedOrigins("/**")
+          .allowedMethods("GET")
+          .maxAge(3600);
+    }
+    
     @Bean
     public HandlerMapping webSocketHandlerMapping() {
         Map<String, WebSocketHandler> map = new HashMap<>();
@@ -36,6 +48,8 @@ public class WebConfig {
         return handlerMapping;
     }
 
+
+    
     @Bean
     public ObjectMapper ObjectMapper() {
         return new ObjectMapper();
