8fe904bfc0b129bd691682a2508fe281639485b2
SPSilicon <dlsrb915@gmail.com>
2023-03-30 19:18:59 +0900

    clean unused import

--- src/main/java/com/spsi/minesweeper/infra/redis/RedisConfig.java
+++ src/main/java/com/spsi/minesweeper/infra/redis/RedisConfig.java
@@ -1,11 +1,9 @@
 package com.spsi.minesweeper.infra.redis;
 
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 import com.redis.om.spring.annotations.EnableRedisDocumentRepositories;
 
