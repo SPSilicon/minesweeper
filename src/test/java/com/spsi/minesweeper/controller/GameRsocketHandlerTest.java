a5634d9b5d5c8603a8b3bed2dfa535363d1407a6
SPSilicon <dlsrb915@gmail.com>
2023-03-30 19:23:13 +0900

    rsocket support

---
+++ src/test/java/com/spsi/minesweeper/controller/GameRsocketHandlerTest.java
@@ -0,0 +1,41 @@
+package com.spsi.minesweeper.controller;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.messaging.rsocket.RSocketRequester;
+
+import com.spsi.minesweeper.domain.Action;
+import com.spsi.minesweeper.domain.Action.ActionType;
+
+import reactor.core.publisher.Flux;
+
+@SpringBootTest
+public class GameRsocketHandlerTest {
+
+    private static RSocketRequester requester;
+
+    @BeforeAll
+    public static void setupOnce(@Autowired RSocketRequester.Builder builder)throws URISyntaxException{
+        requester = builder.websocket(URI.create("ws://localhost:9898"));
+    }
+
+    @Test
+    void testInitGame() {
+        Action action = new Action(12,ActionType.DIG);
+        List<Action> actions  = new ArrayList<>();
+        actions.add(action);
+        Flux<ClientMessage> input = Flux.just(new ClientMessage(null, "123", actions ));
+        Flux<ServerMessage> result = requester.route("game.minesweeper").data(input).retrieveFlux(ServerMessage.class);
+        result.subscribe(System.out::println);
+    }
+}
